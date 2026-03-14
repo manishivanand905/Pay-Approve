ab3e25ee53020d215deafbb7f0cb2f10cd956f23
manishivanand905 <manishivanand905@gmail.com>
2026-03-14 17:01:09 +0530

    Update fixes

--- client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders the role selector landing page", () => {
-  render(<App />);
-  expect(
-    screen.getByText(/select your role to continue/i)
-  ).toBeInTheDocument();
-});
